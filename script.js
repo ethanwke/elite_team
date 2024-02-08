2c215b8517ca70380c13c0ba61370fb7ee8d65bb
ethanwke <158763809+ethanwke@users.noreply.github.com>
2024-02-08 17:48:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function link() {
-  window.open("https://www.youtube.com/@ETG_");
+function link(link) {
+  window.open("https://" + link);
+}
+
+function btn_showImgText(imgId, textId) {
+  const img = document.getElementById(imgId);
+  const text = document.getElementById(textId);
+  if (img.style.display === "none") {
+    img.style.display = 'inline';
+    text.style.display = "none";
+  }
+  else {
+    img.style.display = 'none';
+    text.style.display = "inline";
+  }
 }
